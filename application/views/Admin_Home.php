a5d48d1d0aa5bba6a78a11cb22b098f48d7c97e3
Sulaiman Osman <regrizzlyy@gmail.com>
2018-03-20 18:29:01 +0700

    change login, add home

--- application/views/Admin_Home.php
+++ application/views/Admin_Home.php
@@ -1,32 +1,73 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="utf-8">
-	<title>PintuMagang</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="<?=base_url();?>assets/css/Admin_Home_Navbar.css" rel="stylesheet" />
 </head>
 <body>
 
-	<div>
-    <div class="col-md-1"></div>
-    <div class="col-md-10">
+<div class="container base">
+  <div class="row div">
+    <div class="container top">
+      <nav class="navbar navbar-inverse">  
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span> 
+            </button>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                <li><a href="" target="_blank"><i class="fa fa-user"></i>Sulaiman osman</a></li>
+                <li><a href=""><i class="glyphicon glyphicon-log-out"></i> Logout</a></li>
+                </ul>
+          </div>  
+      </nav>
+    </div>
+  <div class="container content">
+    <div class="row div">
+      <div class="col-md-3">
+      <div class="list-group">
+        <a href="" class="list-group-item active"><i class="fa fa-home tab10" aria-hidden="true"></i> Beranda</a>
+        <a href="" class="list-group-item "><i class="fa fa-plus-circle tab10" aria-hidden="true"></i> Admin </a>
+        <a href="" class="list-group-item "><i class="fa fa-file-o tab10" aria-hidden="true"></i> Mahasiswa</a>
+        <a href="" class="list-group-item "><i class="fa fa-file-text-o tab10" aria-hidden="true"></i> Perusahaan</a>
+        
+      </div>
+      </div>
+      <div class="col-md-9">
+      <div>
+       <div class="row">
+      
+            <div class="col-xs-6 col-md-3 productbox">
+              <a href="#" class="thumbnail clearfix">
+                <img src="http://localhost/Image/admin.png" width="120px" height="80px" class="img-responsive">
+                <div class="producttitle">Admin</div>
+              </a>
+            </div>
 
-<div class="A">
-		 
-				<div class="deskripsi">
+            <div class="col-xs-6 col-md-3 productbox">
+              <a href="#" class="thumbnail clearfix">
+                <img src="http://localhost/Image/mahasiswa.png" width="120px" height="80px" class="img-responsive">
+                <div class="producttitle">Mahasiswa</div>
+              </a>
+            </div>
 
-					<blockquote>
-							<h1><strong> Selamat datang di Pintu Magang </strong></h1>
-							<p>Selamat Datang <?php echo $this->session->userdata('loger') ?></p>
-							<p>Selamat menajalankan tugas anda dan semoga hari anda menyenangkan.</p>
-						</blockquote>
-					</div>
-			</div>
+            <div class="col-xs-6 col-md-3 productbox">
+              <a href="#" class="thumbnail clearfix">
+                <img src="http://localhost/Image/company.png" width="120px" height="80px" class="img-responsive">
+                <div class="producttitle">Perusahaan</div>
+              </a>
+            </div> 
 
-	<div class="col-md-1"></div>
+       </div>
+      </div>
+      </div>
+    </div>
+  </div>
 </div>
-	
-</div>
-			
+
 
 </body>
 </html>
